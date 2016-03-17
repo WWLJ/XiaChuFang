ca528b08bd706c6794be73c02fc78f30d323cc6d
WuWenJie <826505807@qq.com>
2016-03-17 15:10:26 +0800

    增加类似.pch的文件
    主要存放一些宏定义的东西

---
+++ XiaChuFang/XiaChuFang/PrefixHeader.swift
@@ -0,0 +1,17 @@
+//
+//  PrefixHeader.swift
+//  XiaChuFang
+//
+//  Created by YiTie on 16/3/17.
+//  Copyright © 2016年 EasyFlower. All rights reserved.
+//
+
+import UIKit
+
+
+let  SCREENWIDTH = UIScreen.mainScreen().bounds.size.width
+let  SCREENHEIGHT =  UIScreen.mainScreen().bounds.size.width
+
+func XCFColor(r:CGFloat, g:CGFloat, b:CGFloat){
+    return UIColor(red: r/255.0, green: g/255.0, blue: b/255.0, alpha: 1)
+}
