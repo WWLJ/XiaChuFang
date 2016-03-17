f92ee77a472c446f69b44e2ca9f8427e4578605a
LJ0223_iOS <572515696@qq.com>
2016-03-17 11:02:29 +0800

    提交代码
    提交代码

--- XiaChuFang/XiaChuFang/Main(下厨房)/View/MainHeaderView.swift
+++ XiaChuFang/XiaChuFang/Main(下厨房)/View/MainHeaderView.swift
@@ -8,21 +8,36 @@
 
 import UIKit
 
+protocol MainHeaderViewDelegate: class {
+    func mainHeaderViewTopBtnClick(sender: UIButton)
+}
+
 class MainHeaderView: UIView {
     
+    var leftBtn    : UIImageView!
+    var rightBtn   : UIImageView!
     
-//    var leftBtn    : UIButton
-//    var rightBtn   : UIButton
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        
-    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        initBaseLayout()
     }
+    
+    func initBaseLayout() {
+        leftBtn = UIImageView.init(frame: CGRectMake(0, 0, UIScreen.mainScreen().bounds.size.width/2, 150))
+        leftBtn.userInteractionEnabled = true
+        leftBtn.backgroundColor = UIColor.cyanColor()
+        let leftTitle = UILabel.init(frame: CGRectMake(15, leftBtn.frame.size.height-30, leftBtn.frame.size.width-25, 15))
+        leftBtn.addSubview(leftTitle)
+        
+        self.addSubview(leftBtn)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     
     func setUpUI() {
         
